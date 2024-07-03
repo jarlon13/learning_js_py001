d9fba4202f2abfce4a9362baa39cdd96198d10c5
Lucas Camilo <lucasspks@gmail.com>
2024-07-03 18:46:32 +0000

    Add javascript variabel and python

---
+++ index.js
@@ -0,0 +1,20 @@
+let num1 = 3
+console.log(num1)
+let  num2 = 4 *4
+console.log(num2)
+let num3 = (1+2) * 3
+console.log(num3)
+
+//Example JavaScript code for Assignment Operators:
+//let myNumber = 10;
+//Example JavaScript code for Arithmetic Operators:
+//let otherNumber = 10 + 2;
+//Example JavaScript for Operator Precedence:
+//let x = 10 + 2 / 2;
+//console.log(x);        // will log 11 to the console
+
+//let y = (10 + 2) / 2;
+//console.log(y);        // will log 6 to the console
+//In particular you will
+//Work with variables
+//console.log values
---
+++ main.py
@@ -0,0 +1,23 @@
+num1 = 3
+print(num1)
+num2 = 4 * 4
+print(num2)
+num3 = (1 + 2 )*3
+print(num3)
+
+
+#Example Python code for Assignment Operators:
+#myNumber = 10
+#Example Python code for Arithmetic Operators:
+#otherNumber = 10 + 2
+#Example Python for Operator Precedence:
+#x = 10 + 2 / 2 
+
+#print(x)      // will print 11 to the terminal
+
+#y = (10 + 2) / 2 
+
+#print(y)      // will print 6 to the terminal
+#In particular, you will
+#Work with variables
+#print values to the terminal
